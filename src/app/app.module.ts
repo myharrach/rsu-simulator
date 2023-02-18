e68629440d14f7ca0df56e6ed0a79625f5bb5c9f
medYassineHR <harrachmin.m.yassine@gmail.com>
2023-02-18 03:42:35 +0100

    add contact and source

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MainComponent } from './components/main/main.component';
-import { MainHomeComponent } from './components/main-home/main-home.component';
+import { SourceComponent } from './components/source/source.component';
 import { TypeZoneComponent } from './components/main/type-zone/type-zone.component';
 import { ZoneUrbanComponent } from './components/main/zone-urban/zone-urban.component';
 import { ZoneRuralComponent } from './components/main/zone-rural/zone-rural.component';
 import {BrowserModule} from "@angular/platform-browser";
 import {Routes, RouterModule} from "@angular/router";
 import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 const routes: Routes = [
-  { path: 'rural', component: ZoneRuralComponent },
-  { path: 'urban', component: ZoneUrbanComponent }
+  { path: '', component: MainComponent,children:[
+
+      { path: 'rural', component: ZoneRuralComponent },
+      { path: 'urban', component: ZoneUrbanComponent },
+    ] },
+  { path: 'source', component: SourceComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
@@ -23,11 +29,12 @@ const routes: Routes = [
     AppComponent,
     NavBarComponent,
     MainComponent,
-    MainHomeComponent,
+    SourceComponent,
     TypeZoneComponent,
     ZoneUrbanComponent,
     ZoneRuralComponent,
-    FooterComponent
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
