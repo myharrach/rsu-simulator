d1d45f81e9b5a1e0f7beb0169c4307cefec129f5
medYassineHR <harrachmin.m.yassine@gmail.com>
2023-02-18 15:28:08 +0100

    fix padding and test routing with redirects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes,{useHash: true} )
+    RouterModule.forRoot(routes, )
   ],
   providers: [],
   bootstrap: [AppComponent]
